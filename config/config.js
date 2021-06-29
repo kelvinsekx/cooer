4504878ab9ae3a9a8416a840de4b39811a3e831f
kelvinsekx <utdkelvin@gmail.com>
2021-06-30 00:08:29 +0100

    builds..

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     env: process.env.NODE_ENV || "development",
-    port: process.env.PORT || 4006,
+    port: process.env.PORT || 4066,
     jwtSecret: process.env.JWT_SECRET || "2fdDFG3353eegdDGGFHDGJvf",
     mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST 
-    ||  "mongodb+srv://utdkelvin:kukuejubola1997@cluster0.zbmj6.mongodb.net/socmedia?retryWrites=true&w=majority" 
-    //|| "mongodb://localhost:27017/onstreet"
+    //||  "mongodb+srv://utdkelvin:kukuejubola1997@cluster0.zbmj6.mongodb.net/socmedia?retryWrites=true&w=majority" 
+    || "mongodb://localhost:27017/cooer"
 };
 
 export default CONFIG;
