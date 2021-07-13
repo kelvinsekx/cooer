c71df60f3876e5ca7a88c9f11baee45c6ec70a5c
kelvinsekx <utdkelvin@gmail.com>
2021-07-13 11:40:31 +0100

    replaced jwt localStorage handler with cookie in browser

--- client/helpers/auth.helper.js
+++ client/helpers/auth.helper.js
@@ -1,5 +1,44 @@
 import { SIGNOUT } from "../apis/auth/api-auth";
 
+
+function deleteCookie(name) {
+  setCookie(name, "", {
+    'max-age': -1
+  })
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    // add other defaults here if necessary
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
 const AUTH = {
     /**
      * 
@@ -13,7 +52,8 @@ const AUTH = {
      */
     authenticate(jwt, cb) {
         if(typeof window !== 'undefined'){
-            sessionStorage.setItem('jwt', JSON.stringify(jwt))
+            setCookie('jwt', JSON.stringify(jwt),
+		    {secure: true, 'max-age': 300600, samesite:"strict"})
         }
         cb()
     },
@@ -23,8 +63,8 @@ const AUTH = {
             return false
         }
         let item = false;
-        if(sessionStorage.getItem('jwt')){
-         item = JSON.parse(sessionStorage.getItem("jwt"));
+        if(getCookie('jwt')){
+         item = JSON.parse(getCookie("jwt"));
             return item;
         }else {
             return item;
@@ -33,8 +73,8 @@ const AUTH = {
 
     clearJWT(cb) {
         if (typeof window !== 'undefined'){
-            sessionStorage.removeItem("jwt")
-        }
+         deleteCookie("jwt")
+	}
         cb()
         SIGNOUT().then(data=> {
             document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC path=/;"
